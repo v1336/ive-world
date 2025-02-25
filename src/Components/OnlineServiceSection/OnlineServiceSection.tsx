d1503492e8d4bc2eea964dbff4534f40fdd328f2
gigabelka <gigabelka@gmail.com>
2025-02-25 18:39:05 +0500

    Правлю вёрстку.

--- src/Components/OnlineServiceSection/OnlineServiceSection.tsx
+++ src/Components/OnlineServiceSection/OnlineServiceSection.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import styles from "./OnlineServiceSection.module.css";
 
-const OnlineServiceSection: React.FC = () => {
+const OnlineService: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.contentWrapper}>
-        <h2 className={styles.title}>Услуга онлайн</h2>
-        <p className={styles.description}>
-          Оставьте свой запрос на сайте www.ivelabs.ru, и наш специалист
-          свяжется с вами в кратчайшие сроки!
-        </p>
+      <div className={styles.contentContainer}>
+        <div className={styles.textContent}>
+          <h1 className={styles.title}>{"Услуга онлайн"}</h1>
+          <p className={styles.description}>
+            {
+              "Оставьте свой запрос на сайте www.ivelabs.ru, и наш специалист свяжется с вами в кратчайшие сроки!"
+            }
+          </p>
+        </div>
       </div>
-      <button
-        className={styles.requestButton}
-        onClick={() => console.log("Request button clicked")}
-      >
-        Оставить запрос
-      </button>
+      <div className={styles.section}>
+        <div className={styles.divSection}></div>
+        <div className={styles.bottomSection}>
+          {/* <div className={styles.groupImage}></div> */}
+          <button
+            className={styles.requestButton}
+            onClick={() => console.log("Request button clicked")}
+          >
+            {"Оставить запрос"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default OnlineServiceSection;
+export default OnlineService;
