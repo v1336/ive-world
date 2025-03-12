c9efd4e1edf25786175a7c8071e467fdff176c1d
gigabelka <gigabelka@gmail.com>
2025-03-12 13:52:50 +0500

    Добавил уведомления

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -7,9 +7,53 @@ import MaintenanceSection from "./Components/MaintenanceSection/MaintenanceSecti
 import MeasurementSection from "./Components/MeasurementSection/MeasurementSection";
 import styles from "./Layout.module.css";
 
+// async function notifyUser() {
+//   await Notification.requestPermission().then((permission) => {
+//     if (permission === "granted") {
+//       console.log("Enabling notifications");
+//     } else if (permission === "denied") {
+//       alert("Notifications is denied");
+//     }
+//   });
+// }
+
+function notifyUser() {
+  if (!("Notification" in window)) {
+    // Check if the browser supports notifications
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    const notification = new Notification("Hi there!");
+    // …
+  } else if (Notification.permission !== "denied") {
+    // We need to ask the user for permission
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        const notification = new Notification("Hi there!");
+        // …
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them anymore.
+}
+
 const Layout: React.FC = () => {
+  notifyUser();
+
+  const buttonClick = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const notification = new Notification("Enabling notifications");
+  };
+
   return (
     <div className={styles.layout}>
+      <button onClick={buttonClick} className="button">
+        Hello world.
+      </button>
       <HeaderSection />
       <ServiceSection />
       <OnlineServiceSection />
