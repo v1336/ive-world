adea93184027a7a44adb00bb81c934cdbf6aa9aa
gigabelka <gigabelka@gmail.com>
2025-05-12 16:48:11 +0500

    Добавил коммит для GH-pages

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ npm i
 npm run dev
 npm run build
 npm run deploy
+
+https://vite.dev/config/
+Для GH-pages
+base: "/ive-world/",
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Для GH-pages
   base: "/ive-world/",
   plugins: [react()],
   server: {
